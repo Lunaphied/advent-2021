bf5dedcb46a987273409ba9749090bb4f01668e7
Irides <irides@irides.network>
2021-12-04 00:02:00 +0100

    day1: Clean up a bit

--- src/day1.exs
+++ src/day1.exs
@@ -1,9 +1,8 @@
 {:ok, lines} = File.read("data/day1")
 lines |> String.trim |> String.split("\n")
 |> Stream.filter(&String.length(&1) > 0)
-|> Enum.map(&String.to_integer/1)
-|> Stream.chunk_every(2, 1)
-|> Enum.filter(fn [a, b | _] -> b>a; [_] -> false;  end)
-|> Enum.count
+|> Stream.map(&String.to_integer/1)
+|> Stream.chunk_every(2, 1, :discard)
+|> Enum.count(fn [h,t|_] -> t > h end)
 |> Integer.to_string
 |> IO.puts
